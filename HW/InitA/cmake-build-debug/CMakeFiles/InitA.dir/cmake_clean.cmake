5549e249cc98340f9e7d9591f45b3145f3b1f8a1
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2021-09-25 21:22:09 +0200

    Adding InitA

---
+++ HW/InitA/cmake-build-debug/CMakeFiles/InitA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InitA.dir/main.cpp.obj"
+  "InitA.exe"
+  "InitA.exe.manifest"
+  "InitA.pdb"
+  "libInitA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InitA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW/InitA/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, cuphuon3" << std::endl;
+    return 0;
+}
