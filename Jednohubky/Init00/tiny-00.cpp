eff3231afbd97b3085e4d5492d034afed4d2a81c
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2021-09-30 22:11:26 +0200

    Adding init00

--- Jednohubky/Init00/tiny-00.cpp
+++ Jednohubky/Init00/tiny-00.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 void write_stats(vector<double> const& data, ostream& out) {
-    double min = numeric_limits<double>::max();
-    double max = - numeric_limits<double>::max();
+    double min = numeric_limits<double>::infinity();
+    double max = - numeric_limits<double>::infinity();
 
     for (double x : data) {
         if (min > x){min = x;}
         if (max < x){max = x;}
     }
 
-    double sum;
+    double sum = 0;
 
-    for (double y:data) {sum = sum + y;}
+    for (double y : data) {sum += y;}
 
     out << setprecision(2) << fixed <<"min: "<< min <<'\n'<<"max: "<< max <<'\n'<<"mean: "<< sum/data.size() << endl;
 }
